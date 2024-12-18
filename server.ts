83a2ef516746bb0162ac2ca61cb92679df1810fd
Jonathan Wren <jonathan.wren@higherme.app>
2024-12-18 09:37:18 +0000

    Updates to get it to build on heroku

--- server.ts
+++ server.ts
@@ -1,8 +1,8 @@
-import express from "express";
-import { createServer } from "http";
-import { Server } from "socket.io";
-import cors from "cors";
-import { SpeechClient } from "@google-cloud/speech";
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+import { SpeechClient } from '@google-cloud/speech';
 
 const app = express();
 app.use(cors());
@@ -16,7 +16,7 @@ const io = new Server(httpServer, {
 });
 
 const speechClient = new SpeechClient({
-  credentials: JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS || "'{}'"),
+  credentials: JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS || '{}'),
 });
 
 io.on("connection", (socket) => {
@@ -28,19 +28,19 @@ io.on("connection", (socket) => {
     recognizeStream = speechClient
       .streamingRecognize({
         config: {
-          encoding: "WEBM_OPUS",
+          encoding: 'WEBM_OPUS',
           sampleRateHertz: 48000,
-          languageCode: "en-US",
+          languageCode: 'en-US',
           enableAutomaticPunctuation: true,
-          model: "latest_short",
+          model: 'latest_short',
         },
         interimResults: true,
       })
-      .on("error", console.error)
-      .on("data", (data) => {
+      .on('error', console.error)
+      .on('data', (data) => {
         const result = data.results[0];
         if (result) {
-          socket.emit("transcription", result.alternatives[0].transcript);
+          socket.emit('transcription', result.alternatives[0].transcript);
         }
       });
   });
